369f5f3532bff9adc6b8227c06ba87f78f08e5ee
Joachim Bengtsson <joachimb@gmail.com>
2014-10-23 23:18:15 +0200

    Initial Commit

---
+++ OcuCam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OcuCam
+//
+//  Created by Joachim Bengtsson on 2014-10-23.
+//  Copyright (c) 2014 Thirdcog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
